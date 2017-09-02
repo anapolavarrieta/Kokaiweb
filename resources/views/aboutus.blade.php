64457bdd35045911ba733ca69f2d4694d7fefafa
Gaby OP <gaby@kokai.com.mx>
2017-09-01 19:39:46 -0500

    Mejoras en textos de servicios y nostros

--- resources/views/aboutus.blade.php
+++ resources/views/aboutus.blade.php
@@ -4,27 +4,27 @@
 <div id="section1">
  <div class="container-fluid">
  	<div class="row">
-  			<div class= "col-xs-12 col-md-6" id="abouttext">
+  			<div class= "col-xs-12 col-md-4" id="abouttext">
   				<img class="img-responsive" id="imgabout" src= "{{URL::asset('/images/Logoblanco.jpg')}}" alt="About us"/></a>
   			</div>
-  			<div class= "col-xs-12 col-md-6" id="abouttext">
+  			<div class= "col-xs-12 col-md-8" id="abouttext">
   				<p> Somos un grupo de desarrolladores buscando resolver diferentes necesidades tecnológicas (TI)</p>
 
 				<p> Nuestro objetivo es ayudar a pequeñas y medianas empresas a crecer su negocio por medio de:</p>
 
 				<ul>
-					<li> <b>A</b>segurar que la solución se adapta a sus necesidades. </li>
-					<li> <b>C</b>orroborar que los clientes toman el mejor provecho de las soluciones.</li>
-					<li> <b>C</b>ertificar que mientras mayor sea la funcionalidad del sitio, mayor sea el rendimiento de la empresa. </li>
-					<li> <b>E</b>ntender no solo las necesidades técnicas de nuestros clientes sino sus necesidades reales. </li>
-					<li> <b>D</b>esarrollar su sitio web o aplicación, permitiendo que los usuarios se sientan cómodos y más productivos utilizando las herramientas.</li>
-					<li> <b>A</b>yudar a nuestros clientes a interpretar la información (Big Data). </li>
+					
+					<P> <b>I</b>ncrementar el rendimiento de la empresa al implementar las soluciones.</P>
+					<P> <b>D</b>esarrollar sitios web o aplicaciones donde los usuarios se sientan cómodos y más productivos utilizando las herramientas.</P>
+					<P> <b>E</b>ntender las necesidades reales de nuestros clientes y no solo las necesidades técnicas.</P>
+					<P> <b>A</b>provechar al máximo los sistemas e información de los negocios de nuestros clientes (Big Data)..</P>
+					<P> <b>S</b>oluciones adaptadas a sus necesidades.</P>
 					
 				</ul>
 
-				<p> Para resolver las necesidades creamos soluciones especializadas y hacemos análisis de datos. </p>
+				<!--p> Para resolver las necesidades creamos soluciones especializadas y hacemos análisis de datos. </p>
 
-				<!--p> El perfil de las fundadoras de KOKAI WEB </p>
+				<p> El perfil de las fundadoras de KOKAI WEB </p>
 
 				<ul>
 					<li> Ana Paula Olavarrieta es Ingeniera en Negocios certificada por la Universidad de Harvard en Desarrollo Web. </li>
--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -6,26 +6,30 @@
  		<div class="row" id="serviciostext">
  			<div class= "col-xs-12 col-md-12" id="serviciostext">
  				<div class="row">
- 					<div class= "col-xs-12 col-md-0"></div>
  					<div class= "col-xs-12 col-md-2">
  						<img class="img-responsive" id="imgproyecto" src= "{{URL::asset('/images/Abeja.jpg')}}" alt="About us"/>
  					</div>
  					<div class= "col-xs-12 col-md-4">
  						<h2 class="servicioh2">Desarrollo Web</h2>
- 						<p> Cada una de las páginas desarrolladas se adecuan a las necesidades de los negocio, siempre teniendo en cuenta la visibilidad en los diferentes dispositivos (celulares, tablets, computadoras, etc...).</p>
- 						<p> Nos acoplamos a cualquier CMS (Wordpress, Joomla, Drupal) y utilizamos los frameworks Laravel (PHP) y Django (PYTHON).</p>
+ 						<p> Desarrollamos páginas de acuerdo con a las necesidades de los negocios,.</p>
+ 						<p> La tecnología que utilizamos se adecua a las necesidades del desarrollo.</p> 
+						<ul>
+							<p>CMS: Wordpress, Joomla y Drupal</p> 
+							<p>Frameworks: Laravel (PHP) y Django (PYTHON).</p>
+						</ul>
  					</div>
- 					<div class= "col-xs-12 col-md-2">
+ 					<div class= "col-xs-0 col-md-2">
  						<img class="img-responsive" id="imgproyecto" src= "{{URL::asset('/images/Abeja.jpg')}}" alt="About us"/>
  					</div>
  					<div class= "col-xs-12 col-md-4">
  						<h2 class="servicioh2">Diseño Web</h2>
  						<p> Diseñamos páginas web para todo tipo de sitios, dandole un toque único y original a cada página. </p>
+						<p> Buscamos siempre tener una buena visibilidad en los diferentes dispositivos (celulares, tablets, computadoras, etc...)</p>
  					</div>
  				</div>
  			</div>
 
- 			<div class= "col-xs-12 col-md-12" >
+ 			<!--div class= "col-xs-12 col-md-12" >
  				<div class="row">
  					<div class= "col-xs-12 col-md-6"></div>
  					<div class= "col-xs-12 col-md-2">
@@ -35,36 +39,31 @@
  					</div>
  					<div class= "col-xs-12 col-md-0"></div>
  				</div>
- 			</div>
+ 			</div-->
+		
 
  			<div class= "col-xs-12 col-md-12" id="serviciostext">
  				<div class="row">
  					<div class= "col-xs-12 col-md-0"></div>
- 					<div class= "col-xs-12 col-md-2">
- 						<img class="img-responsive" id="imgproyecto" src= "{{URL::asset('/images/Abeja.jpg')}}" alt="About us"/>
- 					</div>
- 					<div class= "col-xs-12 col-md-4">
- 						<h2 class="servicioh2">Automatización de procesos</h2>
- 						<p> Reducimos el tiempo que utilizan las empresas en procesos que, normalmente, se hacen manuales. La mejora y la automatización de procesos ayuda a los negocios a tener precisión en los mismos y disminuir errores.</p>
- 					</div>
- 					<div class= "col-xs-12 col-md-6"></div>
- 				</div>
- 			</div>
-
- 			<div class= "col-xs-12 col-md-12" id="serviciostext">
- 				<div class="row">
- 					<div class= "col-xs-12 col-md-6"></div>
  					<div class= "col-xs-12 col-md-2">
  						<img class="img-responsive" id="imgproyecto" src= "{{URL::asset('/images/Abeja.jpg')}}" alt="About us"/>
  					</div>
  					<div class= "col-xs-12 col-md-4">
  						<h2 class="servicioh2">Tienda en línea</h2>
- 						<p> Diseñamos y desarrollamos tiendas en línea, de acuerdo a las necesidades de venta.</p>
+ 						<p> Creamos tiendas en línea, de acuerdo a las necesidades de venta.</p>
+						<p> Gracias a nuestra experiencia con bases de datos, podemos automatizar la sincronización de inventarios</p>
  					</div>
- 					<div class= "col-xs-12 col-md-0"></div>
+ 					<div class= "col-xs-12 col-md-2">
+ 						<img class="img-responsive" id="imgproyecto" src= "{{URL::asset('/images/Abeja.jpg')}}" alt="About us"/>
+ 					</div>
+ 					<div class= "col-xs-12 col-md-4">
+ 						<h2 class="servicioh2">Automatización de procesos</h2>
+ 						<p> Reducimos el tiempo de procesos que, normalmente, se hacen manuales.</p> 
+						<p> La mejora y automatización de procesos ayuda a los negocios a tener mayor precisión y disminuir errores.</p>
+ 					</div>
  				</div>
  			</div>
- 		
+ 		
 </div>
 
 
