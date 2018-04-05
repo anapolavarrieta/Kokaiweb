d382880cab0c8432306ddeab912dab8fefb016b5
gabyKOKAI <gaby@kokai.com.mx>
2018-04-05 10:19:07 -0500

    Cambios Logos clientes

--- resources/views/clientes.blade.php
+++ resources/views/clientes.blade.php
@@ -30,26 +30,30 @@
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-3 col-md-3 divLogoC vertical-center">
-                        <a href="https://www.landproyectos.com.mx/" target="_blank">
-                            <img class="imgLogo" src= "{{URL::asset('/images/logo_land.jpg')}}"/>
-                        </a>
-                    </div>
+                    <div class="col-xs-12 col-md-6">
+                        <div class="col-xs-6 col-md-3 divLogoC vertical-center">
+                            <a href="https://www.landproyectos.com.mx/" target="_blank">
+                                <img class="imgLogo" src= "{{URL::asset('/images/logo_land.jpg')}}"/>
+                            </a>
+                        </div>
 
-                    <div class="col-xs-3 col-md-3 divLogoC vertical-center">
-                        <a href="https://www.mipar.mx/" target="_blank">
-                            <img class="imgLogo" src= "{{URL::asset('/images/logo_gonpard.png')}}"/>
-                        </a>
+                        <div class="col-xs-6 col-md-3 divLogoC vertical-center">
+                            <a href="https://www.mipar.mx/" target="_blank">
+                                <img class="imgLogo" src= "{{URL::asset('/images/logo_gonpard.png')}}"/>
+                            </a>
+                        </div>
                     </div>
-                    <div class="col-xs-3 col-md-3 divLogoC vertical-center">
-                        <a href="http://www.coordinadoradeeventos.com/" target="_blank">
-                            <img class="imgLogo" src= "{{URL::asset('/images/logo_coordinadora.png')}}"/>
-                        </a>
-                    </div>
-                    <div class="col-xs-3 col-md-2 divLogoC vertical-center">
-                        <a href="http://www.paketealivianes.org/" target="_blank">
-                            <img class="imgLogo" src= "{{URL::asset('/images/logo_pakete.png')}}"/>
-                        </a>
+                    <div class="col-xs-12 col-md-6">
+                        <div class="col-xs-6 col-md-3 divLogoC vertical-center">
+                            <a href="http://www.coordinadoradeeventos.com/" target="_blank">
+                                <img class="imgLogo" src= "{{URL::asset('/images/logo_coordinadora.png')}}"/>
+                            </a>
+                        </div>
+                        <div class="col-xs-4 col-md-2 divLogoC vertical-center">
+                            <a href="http://www.paketealivianes.org/" target="_blank">
+                                <img class="imgLogo" src= "{{URL::asset('/images/logo_pakete.png')}}"/>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -80,11 +84,15 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-xs-3 col-md-3 divLogoC vertical-center">
-                            <img class="imgLogo" src= "{{URL::asset('/images/logo_contravel.png')}}"/>
+                        <div class="col-xs-12 col-md-3">
                         </div>
-                        <div class="col-xs-3 col-md-3 divLogoC vertical-center">
-                            <img class="imgLogo" src= "{{URL::asset('/images/logo_mmdi.png')}}"/>
+                        <div class="col-xs-12 col-md-6">
+                            <div class="col-xs-3 col-md-3 divLogoC vertical-center">
+                                <img class="imgLogo" src= "{{URL::asset('/images/logo_contravel.png')}}"/>
+                            </div>
+                            <div class="col-xs-3 col-md-3 divLogoC vertical-center">
+                                <img class="imgLogo" src= "{{URL::asset('/images/logo_mmdi.png')}}"/>
+                            </div>
                         </div>
                     </div>
                 </div>
